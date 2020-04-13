4dbd2f1ba7ba5f8e1df05bf1d113fa96a3d2aeb9
xiechangwang <1113868253@qq.com>
2020-04-13 18:00:51 +0800

    renderRouters

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {Provider} from 'react-redux';
-import { BrowserRouter,Route } from 'react-router-dom';
+import { renderRoutes } from 'react-router-config'
+import { BrowserRouter} from 'react-router-dom';
 import {Globalstyle} from './style.js';
 import {Iconfont} from './statics/iconfont/iconfont.js';
 //header
@@ -8,6 +9,19 @@ import Header from './common/header/index';
 import Home from './pages/home';
 import Detail from './pages/detail';
 import store from './store';
+
+const routers=[
+  {
+    path:'/',
+    exact:true,
+    component:Home
+  },
+  {
+    path:'/detail',
+    exact:true,
+    component:Detail
+  }
+]
 
 function App() {
   return (
@@ -18,8 +32,9 @@ function App() {
       <BrowserRouter>
         <div>
           <Header/>
-          <Route path="/" exact component={Home}></Route>
-          <Route path="/detail" exact component={Detail}></Route>
+          {/* <Route path="/" exact component={Home}></Route>
+          <Route path="/detail" exact component={Detail}></Route> */}
+          {renderRoutes(routers)}
         </div>
       </BrowserRouter>
       </div>
